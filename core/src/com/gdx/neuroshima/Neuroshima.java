b0bc5baf5d0dc98db0d4f32620884c392231d294
Kuba <jakub.gulbinowicz@gmail.com>
2023-03-29 17:47:33 +0200

    setup update - full screen added, java version changed to 17

--- core/src/com/gdx/neuroshima/Neuroshima.java
+++ core/src/com/gdx/neuroshima/Neuroshima.java
@@ -1,11 +1,13 @@
 package com.gdx.neuroshima;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Neuroshima extends ApplicationAdapter {
+public class Neuroshima extends Game
+{
 
 	
 	@Override
--- desktop/src/com/gdx/neuroshima/DesktopLauncher.java
+++ desktop/src/com/gdx/neuroshima/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.gdx.neuroshima;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.gdx.neuroshima.Neuroshima;
@@ -9,7 +10,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("neuroshima-hex-GDX");
+		config.setTitle("Neuroshima");
+		config.setWindowedMode(1920, 980);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		new Lwjgl3Application(new Neuroshima(), config);
 	}
 }
